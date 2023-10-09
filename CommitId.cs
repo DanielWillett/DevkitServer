474d5a7441ccbb624f881c5273a3aa2167b16fba
Danny Willett <dannywill8@gmail.com>
2023-10-09 02:49:48 -0400

    ignore commitid

--- CommitId.cs
+++
@@ -1,7 +0,0 @@
-namespace DevkitServer;
-
-// This file is automatically overwritten during commit.
-internal static class CommitId
-{
-public static readonly string Commit = "42598c9de5d060fe5d53287f10f93b8c9701ef00";
-}
