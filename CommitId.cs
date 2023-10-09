95b1923c5d4743e3dc5aa1c55e0d098d71a85a60
Daniel Willett <12886600+DanielWillett@users.noreply.github.com>
2023-10-09 02:48:23 -0400

    Update CommitId.cs

--- CommitId.cs
+++ CommitId.cs
@@ -3,5 +3,5 @@ namespace DevkitServer;
 // This file is automatically overwritten during commit.
 internal static class CommitId
 {
-public static readonly string Commit = "35856f2053bfed4c916f30a9449daa92eb54c339";
+public static readonly string Commit = "0000000000000000000000000000000000000000";
 }
