e9139a25cd1c192259a70cdf815f9ec2c9c3dd08
Danny Willett <dannywill8@gmail.com>
2023-10-09 02:57:11 -0400

    test commit

--- Multiplayer/Levels/ReplicatedLevelDataRegistry.cs
+++ Multiplayer/Levels/ReplicatedLevelDataRegistry.cs
@@ -26,7 +26,6 @@ public static class ReplicatedLevelDataRegistry
             RegisteredTypes.Clear();
         }
     }
-
     internal static void RegisterFromAssembly(Assembly assembly, IList<ReplicatedLevelDataSourceInfo>? info, PluginAssembly? plugin)
     {
         lock (RegisteredTypes)
