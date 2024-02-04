33ede9fbe2358edc90e0e62eda68a059a3d0f57a
Daniel Willett <dannywill8@gmail.com>
2024-02-04 00:43:18 -0500

    Added extra documentation and thread checking.

--- Util/LightingUtil.cs
+++ Util/LightingUtil.cs
@@ -75,6 +75,7 @@ public static class LightingUtil
         CallUpdateLightingTime?.Invoke();
         return true;
     }
+#endif
 
     /// <summary>
     /// Gets an English display string for a time's color value at the given index.
@@ -114,58 +115,114 @@ public static class LightingUtil
             _ => index.ToString(CultureInfo.InvariantCulture)
         };
     }
-#endif
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.azimuth"/> and updates the UI.
+    /// </summary>
+    /// <remarks><paramref name="azimuthDeg"/> will be converted into a 0° to 360° range.</remarks>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetAzimuthLocal(float azimuthDeg)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.azimuth = (azimuthDeg % 360 + 360) % 360;
 #if CLIENT
         LightingPatches.UpdateAzimuth();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.bias"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetBiasLocal(float bias)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.bias = bias;
 #if CLIENT
         LightingPatches.UpdateBias();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.fade"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetFadeLocal(float fade)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.fade = fade;
 #if CLIENT
         LightingPatches.UpdateFade();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.rainDur"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetRainDurationLocal(float rainDurationSec)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.rainDur = rainDurationSec;
 #if CLIENT
         LightingPatches.UpdateRainDuration();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.rainFreq"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetRainFrequencyLocal(float rainFrequencySec)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.rainFreq = rainFrequencySec;
 #if CLIENT
         LightingPatches.UpdateRainFrequency();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.snowDur"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetSnowDurationLocal(float snowDurationSec)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.snowDur = snowDurationSec;
 #if CLIENT
         LightingPatches.UpdateSnowDuration();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.snowFreq"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetSnowFrequencyLocal(float snowFrequencySec)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.snowFreq = snowFrequencySec;
 #if CLIENT
         LightingPatches.UpdateSnowFrequency();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.seaLevel"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetSeaLevelLocal(float seaLevel)
     {
+        ThreadUtil.assertIsGameThread();
+
 #if SERVER
         // setting sea level on server throws an exception because it tries to update bubbles :/
         if (SetSeaLevelUnderlyingField != null)
@@ -180,48 +237,95 @@ public static class LightingUtil
         LightingPatches.UpdateSeaLevel();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.snowLevel"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetSnowLevelLocal(float snowLevel)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.snowLevel = snowLevel;
 #if CLIENT
         LightingPatches.UpdateSnowLevel();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.time"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetTimeLocal(float time)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.time = time;
 #if CLIENT
         LightingPatches.UpdateTime();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.moon"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetMoonCycleLocal(byte moonCycle)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.moon = moonCycle;
 #if CLIENT
         LightingPatches.UpdateMoonCycle();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.canRain"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetCanRainLocal(bool canRain)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.canRain = canRain;
 #if CLIENT
         LightingPatches.UpdateCanRain();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets <see cref="LevelLighting.canSnow"/> and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetCanSnowLocal(bool canSnow)
     {
+        ThreadUtil.assertIsGameThread();
+
         LevelLighting.canSnow = canSnow;
 #if CLIENT
         LightingPatches.UpdateCanSnow();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets the preview weather asset and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetPreviewWeatherAssetLocal(WeatherAssetBase? asset, float blendAlpha = 0f)
     {
+        ThreadUtil.assertIsGameThread();
+
         LightingManager.ReceiveLightingActiveWeather(asset?.GUID ?? Guid.Empty, blendAlpha, default);
 #if CLIENT
         LightingPatches.UpdateWeatherAsset();
 #endif
     }
+
+    /// <summary>
+    /// Locally sets a color value at a specific time and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetTimeColor(ELightingTime time, int index, Color color)
     {
         if (!CheckTimeColorSafe(time, index))
@@ -232,6 +336,11 @@ public static class LightingUtil
         LightingPatches.UpdateTimeColor(time, index);
 #endif
     }
+
+    /// <summary>
+    /// Locally sets a single value at a specific time and updates the UI.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static void SetTimeSingle(ELightingTime time, int index, float single)
     {
         if (!CheckTimeSingleSafe(time, index))
@@ -243,12 +352,25 @@ public static class LightingUtil
 #endif
     }
 
+    /// <summary>
+    /// Checks if a time and index of a color value is safe.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static bool CheckTimeColorSafe(ELightingTime time, int index)
     {
+        ThreadUtil.assertIsGameThread();
+
         return time >= 0 && index >= 0 && (int)time < LevelLighting.times.Length && index < LevelLighting.times[(int)time].colors.Length;
     }
+
+    /// <summary>
+    /// Checks if a time and index of a single value is safe.
+    /// </summary>
+    /// <exception cref="NotSupportedException">Not on main thread.</exception>
     public static bool CheckTimeSingleSafe(ELightingTime time, int index)
     {
+        ThreadUtil.assertIsGameThread();
+
         return time >= 0 && index >= 0 && (int)time < LevelLighting.times.Length && index < LevelLighting.times[(int)time].singles.Length;
     }
 }
