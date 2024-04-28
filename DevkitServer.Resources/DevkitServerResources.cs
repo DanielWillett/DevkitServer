baf9d5efa1003c6f9e1925cb0fb494346822cc97
Daniel Willett <dannywill8@gmail.com>
2024-04-28 03:46:05 -0400

    bro im done i swear

--- DevkitServer.Resources/DevkitServerResources.cs
+++ DevkitServer.Resources/DevkitServerResources.cs
@@ -158,8 +158,8 @@ internal class DevkitServerResources
 
         new DevkitServerFileResource("DanielWillett.SpeedBytes",
             Lib("DanielWillett.SpeedBytes", "DanielWillett.SpeedBytes.dll"), new Version(0, 0, 32, 0)),
-        new DevkitServerFileResource("DanielWillett.SpeedBytes",
-            Lib("DanielWillett.SpeedBytes", "DanielWillett.SpeedBytes.Unity.dll"), new Version(0, 0, 33, 0)),
+        new DevkitServerFileResource("DanielWillett.SpeedBytes.Unity",
+            Lib("DanielWillett.SpeedBytes", "DanielWillett.SpeedBytes.Unity.dll"), new Version(0, 0, 34, 0)),
         new DevkitServerFileResource("DanielWillett.SpeedBytes.LICENSE",
             Lib("DanielWillett.SpeedBytes", "LICENSE.txt"), new Version(0, 0, 29, 0)),
 
